96e8339cccad3800da0a951abcd48f16551f94a4
Cosmin Popescu <cosminadrianpopescu@gmail.com>
2015-09-09 21:27:28 +0200

    working

--- js/plugin.js
+++ js/plugin.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
             $('#composebody').focus(function(){
                 dim = {w: $('#composebody').width(), h: $('#composebody').height()};
                 scrollerPos = $('scroller.withfooter').position();
+                console.log('pos', scrollerPos);
                 editor(this);
             }).on('vi_quit', function(ev){
                 $(this).width(dim.w).height(dim.h);
