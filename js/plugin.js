6bf4afa56ffe699396934bb9fdea4087e7a5f935
Cosmin Popescu <cosminadrianpopescu@gmail.com>
2015-09-09 21:26:51 +0200

    working

--- js/plugin.js
+++ js/plugin.js
@@ -1,16 +1,16 @@
 var dim = {};
+var scrollerPos = [];
 $(document).ready(function(){
     if ($('input[name="_is_html"]').val() == '0'){
         if (typeof(editor) != 'undefined'){
-            console.log('focus', $('*:focus'));
-            if ($('*:focus').attr('id') == 'composebody'){
-                $('*:focus').blur();
-            }
             $('#composebody').focus(function(){
                 dim = {w: $('#composebody').width(), h: $('#composebody').height()};
+                scrollerPos = $('scroller.withfooter').position();
                 editor(this);
             }).on('vi_quit', function(ev){
                 $(this).width(dim.w).height(dim.h);
+                // Hack for the scroller for the contacts
+                $('.scroller.withfooter').position(scrollerPos);
             });
         }
     }
