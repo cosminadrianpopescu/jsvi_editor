50556dbe9cd6c5b317c6b0e1823e53644d0f693e
Cosmin Popescu <cosminadrianpopescu@gmail.com>
2015-09-09 21:53:13 +0200

    working

--- js/vi.js
+++ js/vi.js
@@ -2245,6 +2245,7 @@ function term_paste(after, ign) {
 	return true;
 }
 function term_keyfix(e) {
+    console.log('key down');
 	if (!e) e = window.event;
 
 	var ch = e.keyCode;
@@ -2289,6 +2290,7 @@ function term_keypress(e) {
 	return false;
 }
 function term_keypress_inner(e, synth) {
+    console.log('key press inner');
 	var k = e.which;
 	var kc;
 	if (e.charCode) {
