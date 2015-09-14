5ba55178f7b055aabdeb9adb1a01c28c99d7e875
Cosmin Popescu <cosminadrianpopescu@gmail.com>
2015-09-14 20:51:10 +0200

    removed console.logs

--- js/plugin.js
+++ js/plugin.js
@@ -1,20 +1,21 @@
 var dim = {};
 var scrollerPos = [];
+
+function init_jsvi(){
+    $('#composebody').focus(function(){
+        dim = {w: $('#composebody').width(), h: $('#composebody').height()};
+        scrollerPos = $('.scroller.withfooter').position();
+        editor(this);
+    }).on('vi_quit', function(ev){
+        $(this).width(dim.w).height(dim.h);
+        // Hack for the scroller for the contacts
+        $('.scroller.withfooter').css('top', scrollerPos.top);
+    });
+}
 $(document).ready(function(){
     if ($('input[name="_is_html"]').val() == '0'){
         if (typeof(editor) != 'undefined'){
-            $('#composebody').focus(function(){
-                dim = {w: $('#composebody').width(), h: $('#composebody').height()};
-                scrollerPos = $('.scroller.withfooter').position();
-                console.log('pos', scrollerPos);
-                editor(this);
-            }).on('vi_quit', function(ev){
-                $(this).width(dim.w).height(dim.h);
-                // Hack for the scroller for the contacts
-                console.log('setting back the position', scrollerPos);
-                $('.scroller.withfooter').css('top', scrollerPos.top);
-                console.log('pos', $('.scroller.withfooter').position());
-            });
+            init_jsvi();
         }
     }
 })
